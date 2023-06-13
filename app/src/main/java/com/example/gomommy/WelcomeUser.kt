33088c49f549470f968c44c96b1504708e0c6f23
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-06-13 23:27:18 +0800

    initial commit for splash screen and welcome user

--- app/src/main/java/com/example/gomommy/WelcomeUser.kt
+++ app/src/main/java/com/example/gomommy/WelcomeUser.kt
@@ -5,18 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class SplashScreen : AppCompatActivity() {
+class WelcomeUser : AppCompatActivity() {
     private val splashDuration: Long = 3000 // 3 seconds
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_welcome)
 
         // Delay using Handler
         Handler().postDelayed(
             {
                 // Start the main activity
-                startActivity(Intent(this, WelcomeUser::class.java))
+                startActivity(Intent(this, TermsAndConditions::class.java))
 
                 // Close the splash activity
                 finish()
