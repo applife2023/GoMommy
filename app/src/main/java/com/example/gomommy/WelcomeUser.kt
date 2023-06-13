24aa6a2edbb4970565de9f36830a72844e0724b1
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-06-13 23:39:41 +0800

    corrected minor error on splashscreen

--- app/src/main/java/com/example/gomommy/WelcomeUser.kt
+++ app/src/main/java/com/example/gomommy/WelcomeUser.kt
@@ -10,7 +10,7 @@ class WelcomeUser : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_welcome_user)
 
         // Delay using Handler
         Handler().postDelayed(
