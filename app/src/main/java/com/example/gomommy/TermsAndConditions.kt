3ece0c286b96e64da714716c34a232ccba7ae432
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-06-14 14:44:44 +0800

    Updated Terms and Condition activity contents

--- app/src/main/java/com/example/gomommy/TermsAndConditions.kt
+++ app/src/main/java/com/example/gomommy/TermsAndConditions.kt
@@ -1,46 +1,64 @@
 package com.example.gomommy
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.gomommy.ui.theme.GoMommyTheme
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class TermsAndConditions : ComponentActivity() {
+class TermsAndConditions : AppCompatActivity() {
+
+    private lateinit var checkbox1: CheckBox
+    private lateinit var checkbox2: CheckBox
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GoMommyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_terms_and_conditions)
+
+        this.checkbox1 = findViewById(R.id.checkbox1)
+        this.checkbox2 = findViewById(R.id.checkbox2)
+        this.button = findViewById(R.id.nextButton)
+
+        // Set a listener for checkbox1
+        checkbox1.setOnCheckedChangeListener { _, _ ->
+            updateButtonEnabledState()
+        }
+
+        // Set a listener for checkbox2
+        checkbox2.setOnCheckedChangeListener { _, _ ->
+            updateButtonEnabledState()
+        }
+
+        // Call the updateButtonEnabledState initially to set the initial state of the yesButton
+        updateButtonEnabledState()
+
+        // Set a click listener for the Next yesButton
+        button.setOnClickListener {
+            if (button.isEnabled) {
+                // Perform the desired action or navigate to the desired activity
+                val intent = Intent(this, LoginAccount::class.java)
+                startActivity(intent)
+
+                finish()
+            } else {
+                // Button is disabled, display a message
+                println("TermsAndConditions, Button is disabled. Showing toast.")
+                Toast.makeText(this, "Please accept the terms and conditions", Toast.LENGTH_SHORT).show()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GoMommyTheme {
-        Greeting("Android")
+    // Function to update the enabled state of the yesButton based on checkbox states
+    private fun updateButtonEnabledState() {
+        button.isEnabled = checkbox1.isChecked && checkbox2.isChecked
+        if (button.isEnabled) {
+            // Change yesButton background color when enabled
+            button.setBackgroundResource(R.drawable.gradient_bg)
+        } else {
+            // Change yesButton background color when disabled
+            button.setBackgroundResource(R.color.disabled_btn)
+        }
     }
 }
