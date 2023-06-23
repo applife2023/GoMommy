9d9970673eff27506e7fcdf8137249e1452190a8
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-06-23 21:46:44 +0800

    Added intent to forgot password

--- app/src/main/java/com/example/gomommy/LoginAccount.kt
+++ app/src/main/java/com/example/gomommy/LoginAccount.kt
@@ -1,5 +1,6 @@
 package com.example.gomommy
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -7,24 +8,44 @@ import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import android.widget.ToggleButton
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.addTextChangedListener
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginAccount : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
     private lateinit var usernameEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
     private lateinit var createAccountButton: Button
-    //private lateinit var forgotPasswordTextView: Button
+    private lateinit var forgotPasswordTextView: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_account)
 
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this , gso)
+
         usernameEditText = findViewById(R.id.createUsernameEditText)
         passwordEditText = findViewById(R.id.createPasswordEditText)
-        //forgotPasswordTextView = findViewById(R.id.forgotPasswordTextView)
+        forgotPasswordTextView = findViewById(R.id.forgotPasswordTextView)
         createAccountButton = findViewById(R.id.loginButton)
         loginButton = findViewById(R.id.createAccountButton)
         loginButton.isEnabled = false // Initially disable the sign-up yesButton
@@ -36,6 +57,12 @@ class LoginAccount : AppCompatActivity() {
 
         passwordEditText.addTextChangedListener { text ->
             updateSignUpButtonState()
+        }
+
+        forgotPasswordTextView.setOnClickListener{
+            val intent = Intent(this, ForgotPassword::class.java)
+            startActivity(intent)
+            finish()
         }
 
         loginButton.setOnClickListener {
@@ -57,6 +84,10 @@ class LoginAccount : AppCompatActivity() {
             val intent = Intent(this, CreateAccount::class.java)
             startActivity(intent)
             finish()
+        }
+
+        findViewById<Button>(R.id.googleSignIn).setOnClickListener{
+            signInGoogle()
         }
     }
 
@@ -85,5 +116,45 @@ class LoginAccount : AppCompatActivity() {
         }
         // Set cursor position to the end of the password field
         passwordEditText.setSelection(passwordEditText.text.length)
+    }
+
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this , MomExperience::class.java)
+                intent.putExtra("email" , account.email)
+                intent.putExtra("name" , account.displayName)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
