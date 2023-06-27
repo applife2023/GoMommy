127d5d982bc26589476694442137b37174b73376
Adrian R. Esguerra <i.am.adriansgrrx@gmail.com>
2023-06-27 23:05:15 +0800

    CreateAccount.kt refactored.

--- app/src/main/java/com/example/gomommy/CreateAccount.kt
+++ app/src/main/java/com/example/gomommy/CreateAccount.kt
@@ -10,8 +10,11 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class CreateAccount : AppCompatActivity() {
 
@@ -23,6 +26,7 @@ class CreateAccount : AppCompatActivity() {
     private lateinit var createAccountButton: Button
     private lateinit var logInTextView: TextView
     //private lateinit var forgotPasswordTextView: Button
+    private lateinit var dbRef: DatabaseReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +39,8 @@ class CreateAccount : AppCompatActivity() {
         confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText)
         logInTextView = findViewById(R.id.logInTextView)
         createAccountButton = findViewById(R.id.createAccountButton)
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Users")
 
         // Add text change listeners to the username and password EditText fields
         usernameEditText.addTextChangedListener {
@@ -49,37 +55,31 @@ class CreateAccount : AppCompatActivity() {
             updateSignUpButtonState()
         }
 
+        confirmPasswordEditText.addTextChangedListener { text ->
+            updateSignUpButtonState()
+        }
+
         createAccountButton.setOnClickListener {
             val username = usernameEditText.text.toString()
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
+            val confirmPassword = confirmPasswordEditText.text.toString()
 
             // Perform sign-up logic here
-<<<<<<< Updated upstream
-            if (username.isNotEmpty() && password.isNotEmpty()){
-                firebaseAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        // Redirect to the desired activity
-                        val intent = Intent(this, MomExperience::class.java)
-                        startActivity(intent)
-                        finish()
-                    }else{
-                        Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
-=======
             if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
                 if(password ==  confirmPassword){
                     firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
                         if (it.isSuccessful) {
                             // Redirect to the desired activity
                             val intent = Intent(this, MomExperience::class.java)
-                            saveUserEmailPassword(email,password)
                             startActivity(intent)
                             finish()
                         } else {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
->>>>>>> Stashed changes
                     }
+                }else{
+                    Toast.makeText(this, "The password does not match!", Toast.LENGTH_SHORT).show()
                 }
             }else{
                 Toast.makeText(this,"Please fill the fields", Toast.LENGTH_SHORT).show()
@@ -110,6 +110,20 @@ class CreateAccount : AppCompatActivity() {
         }
         val showPasswordToggleButton = findViewById<ToggleButton>(R.id.showPasswordToggleButton)
         val passwordEditText = findViewById<EditText>(R.id.createPasswordEditText)
+        val confirmShowPasswordToggleButton = findViewById<ToggleButton>(R.id.confirmShowPasswordToggleButton)
+        val confirmPasswordEditText = findViewById<EditText>(R.id.confirmPasswordEditText)
+        val passwordMismatchTextView = findViewById<TextView>(R.id.passwordMismatchTextView)
+
+        passwordMismatchTextView.isVisible = !isPasswordMatched
+
+        this.confirmPasswordEditText.isEnabled = isPasswordFilled
+        if (this.confirmPasswordEditText.isEnabled) {
+            this.confirmPasswordEditText.setBackgroundResource(R.drawable.custom_edit_text)
+            this.confirmPasswordEditText.setTextColor(Color.parseColor("#FF000000"))
+        } else {
+            this.confirmPasswordEditText.setBackgroundResource(R.drawable.custom_edit_text_disabled)
+            this.confirmPasswordEditText.setTextColor(Color.parseColor("#D2D1D1"))
+        }
 
         showPasswordToggleButton.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -120,7 +134,20 @@ class CreateAccount : AppCompatActivity() {
                 passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
             }
         }
+
+        confirmShowPasswordToggleButton.isEnabled = isConfirmPasswordFilled
+        confirmShowPasswordToggleButton.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked) {
+                // Show password
+                confirmPasswordEditText.transformationMethod = null
+            } else {
+                // Hide password
+                confirmPasswordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+        }
+
         // Set cursor position to the end of the password field
         passwordEditText.setSelection(passwordEditText.text.length)
+        confirmPasswordEditText.setSelection(confirmPasswordEditText.text.length)
     }
 }
