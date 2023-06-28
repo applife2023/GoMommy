bd03edd2fd1d0a822693137df8497d6909531f67
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-06-28 20:54:18 +0800

    added binding method on the Terms and Condition Activity

--- app/src/main/java/com/example/gomommy/TermsAndConditions.kt
+++ app/src/main/java/com/example/gomommy/TermsAndConditions.kt
@@ -6,55 +6,61 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gomommy.databinding.ActivityTermsAndConditionsBinding
 
 class TermsAndConditions : AppCompatActivity() {
 
+    private  lateinit var binding: ActivityTermsAndConditionsBinding
     private lateinit var checkbox1: CheckBox
     private lateinit var checkbox2: CheckBox
     private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms_and_conditions)
+        binding = ActivityTermsAndConditionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         this.checkbox1 = findViewById(R.id.checkbox1)
         this.checkbox2 = findViewById(R.id.checkbox2)
         this.button = findViewById(R.id.nextButton)
 
-        // Set a listener for checkbox1
-        checkbox1.setOnCheckedChangeListener { _, _ ->
+        with(binding) {
+            // Set a listener for checkbox1
+            checkbox1.setOnCheckedChangeListener { _, _ ->
+                updateButtonEnabledState()
+            }
+
+            // Set a listener for checkbox2
+            checkbox2.setOnCheckedChangeListener { _, _ ->
+                updateButtonEnabledState()
+            }
+
+            // Call the updateButtonEnabledState initially to set the initial state of the yesButton
             updateButtonEnabledState()
-        }
 
-        // Set a listener for checkbox2
-        checkbox2.setOnCheckedChangeListener { _, _ ->
-            updateButtonEnabledState()
-        }
+            // Set a click listener for the Next yesButton
+            nextButton.setOnClickListener {
+                if (nextButton.isEnabled) {
+                    // Perform the desired action or navigate to the desired activity
+                    val intent = Intent(this@TermsAndConditions, LoginAccount::class.java)
+                    startActivity(intent)
 
-        // Call the updateButtonEnabledState initially to set the initial state of the yesButton
-        updateButtonEnabledState()
-
-        // Set a click listener for the Next yesButton
-        button.setOnClickListener {
-            if (button.isEnabled) {
-                // Perform the desired action or navigate to the desired activity
-                val intent = Intent(this, LoginAccount::class.java)
-                startActivity(intent)
-
-                finish()
+                    finish()
+                }
             }
         }
     }
 
+
     // Function to update the enabled state of the yesButton based on checkbox states
     private fun updateButtonEnabledState() {
-        button.isEnabled = checkbox1.isChecked && checkbox2.isChecked
-        if (button.isEnabled) {
+        binding.nextButton.isEnabled = binding.checkbox1.isChecked && binding.checkbox2.isChecked
+        if (binding.nextButton.isEnabled) {
             // Change yesButton background color when enabled
-            button.setBackgroundResource(R.drawable.gradient_bg)
+            binding.nextButton.setBackgroundResource(R.drawable.gradient_bg)
         } else {
             // Change yesButton background color when disabled
-            button.setBackgroundResource(R.color.disabled_btn)
+            binding.nextButton.setBackgroundResource(R.color.disabled_btn)
         }
     }
 }
