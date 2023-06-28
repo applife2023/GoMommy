9fd91dc92f82f228d385d80a6f10c12b8832e03f
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-06-28 19:45:58 +0800

    refactored the code using binding method

--- app/src/main/java/com/example/gomommy/SplashScreen.kt
+++ app/src/main/java/com/example/gomommy/SplashScreen.kt
@@ -4,19 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.gomommy.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     private val splashDuration: Long = 3000 // 3 seconds
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Delay using Handler
         Handler().postDelayed(
             {
                 // Start the main activity
-                startActivity(Intent(this, WelcomeUser::class.java))
+                startActivity(Intent(this@SplashScreen, WelcomeUser::class.java))
 
                 // Close the splash activity
                 finish()
