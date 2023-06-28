43a8cfe1943454606a207db4350492fcf391ca71
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-06-28 21:28:01 +0800

    used binding method on the Welcome user activity

--- app/src/main/java/com/example/gomommy/WelcomeUser.kt
+++ app/src/main/java/com/example/gomommy/WelcomeUser.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.gomommy.databinding.ActivityWelcomeBinding
 
 class WelcomeUser : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
     private val splashDuration: Long = 3000 // 3 seconds
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Delay using Handler
         Handler().postDelayed(
