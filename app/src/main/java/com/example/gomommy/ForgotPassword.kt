872a80a659f0d5deaf8c392e9fe0f6e2388faa43
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-06-28 19:12:39 +0800

    refactored the code using binding method

--- app/src/main/java/com/example/gomommy/ForgotPassword.kt
+++ app/src/main/java/com/example/gomommy/ForgotPassword.kt
@@ -5,32 +5,31 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import com.example.gomommy.databinding.ActivityForgotPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPassword : ComponentActivity() {
-    private lateinit var etPassword: EditText
-    private lateinit var resetPassword: Button
-
+    private lateinit var binding: ActivityForgotPasswordBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
-
-        etPassword = findViewById(R.id.resetEmailEditText)
-        resetPassword = findViewById(R.id.resetPasswordButton)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
 
-        resetPassword.setOnClickListener(){
-            val sPassword = etPassword.text.toString()
-            auth.sendPasswordResetEmail(sPassword)
-                .addOnCompleteListener {
-                    Toast.makeText(this, "Please check your email", Toast.LENGTH_SHORT).show()
-                }
-                .addOnCompleteListener {
-                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
-                }
+        with(binding) {
+            resetPasswordButton.setOnClickListener() {
+                val sPassword = resetEmailEditText.text.toString()
+                auth.sendPasswordResetEmail(sPassword)
+                    .addOnCompleteListener {
+                        Toast.makeText(this@ForgotPassword, "Please check your email", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnCompleteListener {
+                        Toast.makeText(this@ForgotPassword, it.toString(), Toast.LENGTH_SHORT).show()
+                    }
+            }
         }
     }
 }
