5bbf9ff2043d6667b37c7b3d0b06dba141661aa5
Adrian R. Esguerra <i.am.adriansgrrx@gmail.com>
2023-06-30 17:26:41 +0800

    New Activity and activity layout added.

---
+++ app/src/main/java/com/example/gomommy/DueDateCalculator.kt
@@ -0,0 +1,11 @@
+package com.example.gomommy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DueDateCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_due_date_calculator)
+    }
+}
