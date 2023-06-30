83a1abc78503e7cc131f0b0a70d0ea776a033ca0
Adrian R. Esguerra <i.am.adriansgrrx@gmail.com>
2023-07-01 00:23:40 +0800

    Save login status in shared preferences logic implemented.

--- app/src/main/java/com/example/gomommy/LoginAccount.kt
+++ app/src/main/java/com/example/gomommy/LoginAccount.kt
@@ -1,15 +1,12 @@
 package com.example.gomommy
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import android.widget.ToggleButton
 import androidx.core.widget.addTextChangedListener
 import com.example.gomommy.databinding.ActivityLoginAccountBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -17,8 +14,6 @@ import com.google.firebase.auth.FirebaseAuth
 class LoginAccount : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: ActivityLoginAccountBinding
-    //private lateinit var forgotPasswordTextView: Button
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,42 +21,44 @@ class LoginAccount : AppCompatActivity() {
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
 
-
-
         with(binding) {
-            loginButton.isEnabled = false // Initially disable the sign-up yesButton
+            loginButton.isEnabled = false // Initially disable the loginButton
 
             // Add text change listeners to the username and password EditText fields
             loginUsernameEditText.addTextChangedListener { text ->
-                updateSignUpButtonState()
+                updateLoginButtonState()
             }
 
             loginPasswordEditText.addTextChangedListener { text ->
-                updateSignUpButtonState()
+                updateLoginButtonState()
             }
 
             loginButton.setOnClickListener {
                 val username = loginUsernameEditText.text.toString()
                 val password = loginPasswordEditText.text.toString()
 
-                // Perform sign-up logic here
+                // Perform login logic here
                 if (username.isNotEmpty() && password.isNotEmpty()) {
                     firebaseAuth.signInWithEmailAndPassword(username, password)
-                        .addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                // Redirect to the desired activity
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                // Save login status in shared preferences
+                                val sharedPrefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+                                val editor = sharedPrefs.edit()
+                                editor.putBoolean("isLoggedIn", true)
+                                editor.apply()
+
+                                // Redirect to the homepage activity
                                 val intent = Intent(this@LoginAccount, Homepage::class.java)
                                 startActivity(intent)
                                 finish()
                             } else {
-                                Toast.makeText(this@LoginAccount, it.exception.toString(), Toast.LENGTH_SHORT)
-                                    .show()
+                                Toast.makeText(this@LoginAccount, task.exception?.message, Toast.LENGTH_SHORT).show()
                             }
                         }
                 } else {
-                    Toast.makeText(this@LoginAccount, "Please fill the fields", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@LoginAccount, "Please fill in all fields", Toast.LENGTH_SHORT).show()
                 }
-
             }
 
             createAccountTextView.setOnClickListener {
@@ -72,14 +69,15 @@ class LoginAccount : AppCompatActivity() {
         }
     }
 
-    private fun updateSignUpButtonState() {
+    private fun updateLoginButtonState() {
         val isUsernameFilled = binding.loginUsernameEditText.text.isNotEmpty()
         val isPasswordFilled = binding.loginPasswordEditText.text.isNotEmpty()
         binding.loginButton.isEnabled = isUsernameFilled && isPasswordFilled
-        if(binding.loginButton.isEnabled){
+
+        if (binding.loginButton.isEnabled) {
             binding.loginButton.setBackgroundResource(R.drawable.log_button)
             binding.loginButton.setTextColor(Color.parseColor("#FE5065"))
-        }else {
+        } else {
             binding.loginButton.setBackgroundResource(R.drawable.disable_button)
             binding.loginButton.setTextColor(Color.parseColor("#D2D1D1"))
         }
