83a9435b354ba785ea75c3e62ea48f071ab25469
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-07-13 23:08:09 +0800

    Update tipsAndAdvice Fragment

--- app/src/main/java/com/example/gomommy/TipsAndAdviceFragment.kt
+++ app/src/main/java/com/example/gomommy/TipsAndAdviceFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
@@ -68,6 +69,18 @@ class TipsAndAdviceFragment : Fragment() {
             }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataInitialize()
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.tipsRecyclerView)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        adapter = TipsItemAdapter(tipsItemArrayList)
+        recyclerView.adapter = adapter
+    }
+
 
 
     private fun dataInitialize(){
@@ -102,7 +115,7 @@ class TipsAndAdviceFragment : Fragment() {
             getString(R.string.tipsItemDescTextView_10)
 
         )
-        
+
         tipsItemSourceTextView = arrayOf(
             getString(R.string.tipsItemSourceTextView_1),
             getString(R.string.tipsItemSourceTextView_2),
