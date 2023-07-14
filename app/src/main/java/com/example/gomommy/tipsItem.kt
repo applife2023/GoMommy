112c823459fca46e62328b52d1c53e6e950327be
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-07-14 10:01:05 +0800

    Update tipsItem.kt

--- app/src/main/java/com/example/gomommy/tipsItem.kt
+++ app/src/main/java/com/example/gomommy/tipsItem.kt
@@ -1,12 +1,13 @@
 package com.example.gomommy
 
+import android.widget.TextView
+
 data class tipsItem(
     val itemTitle: String,
     val itemDesc: String,
     val itemSource: String
-){
+)
 
-    // Empty constructor required for Firebase deserialization
-    constructor() : this("", "", "")
 
-}
+
+
