fbe9f89b46a365c7d6ff5926aa91d4a8ea002310
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-07-16 12:41:53 +0800

    added method for getting locations of nearby hospital

--- app/src/main/java/com/example/gomommy/NearestHospitalFragment.kt
+++ app/src/main/java/com/example/gomommy/NearestHospitalFragment.kt
@@ -4,12 +4,14 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.gomommy.api.NearbyPlacesResponse
 import com.example.gomommy.api.PlacesService
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -19,6 +21,9 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class NearestHospitalFragment : Fragment(), OnMapReadyCallback {
 
@@ -62,8 +67,32 @@ class NearestHospitalFragment : Fragment(), OnMapReadyCallback {
     }
 
     private fun getNearbyHospitals(location: Location) {
+        val apiKey = getString(R.string.google_maps_key)
+        placesService.nearbyPlaces(
+            apiKey = apiKey,
+            location = "${location.latitude},${location.longitude}",
+            radiusInMeters = 2000,
+            placeType = "hospital"
+        ).enqueue(object : Callback<NearbyPlacesResponse> {
+            override fun onResponse(
+                call: Call<NearbyPlacesResponse>,
+                response: Response<NearbyPlacesResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val places = response.body()?.results ?: emptyList()
+                    addHospitalMarkers(places)
+                } else {
+                    Log.e(tag, "Failed to get nearby hospitals")
+                }
+            }
 
+            override fun onFailure(call: Call<NearbyPlacesResponse>, t: Throwable) {
+                Log.e(tag, "Failed to get nearby hospitals", t)
+            }
+        })
     }
+
+
 
     private fun getCurrentLocation(onSuccess: (Location) -> Unit) {
         if (ContextCompat.checkSelfPermission(
