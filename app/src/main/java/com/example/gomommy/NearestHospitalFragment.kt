ff238d15ea43657a22c6e26376f872297bbb872f
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-07-16 12:35:29 +0800

    Updated Nearest Fragment Hospital for searching nearby hospitals

--- app/src/main/java/com/example/gomommy/NearestHospitalFragment.kt
+++ app/src/main/java/com/example/gomommy/NearestHospitalFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.gomommy.api.PlacesService
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -21,6 +22,10 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class NearestHospitalFragment : Fragment(), OnMapReadyCallback {
 
+    private val tag = "NearestHospitalFragment"
+    private val locationPermissionRequestCode = 1
+
+    private lateinit var placesService: PlacesService
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
@@ -31,6 +36,7 @@ class NearestHospitalFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        placesService = PlacesService.create()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
@@ -44,7 +50,8 @@ class NearestHospitalFragment : Fragment(), OnMapReadyCallback {
             == PackageManager.PERMISSION_GRANTED
         ) {
             mMap.isMyLocationEnabled = true
-            getCurrentLocation()
+            getCurrentLocation{ location -> getNearbyHospitals(location)}
+
         } else {
             ActivityCompat.requestPermissions(
                 requireActivity(),
@@ -54,7 +61,11 @@ class NearestHospitalFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
-    private fun getCurrentLocation() {
+    private fun getNearbyHospitals(location: Location) {
+
+    }
+
+    private fun getCurrentLocation(onSuccess: (Location) -> Unit) {
         if (ContextCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -72,6 +83,7 @@ class NearestHospitalFragment : Fragment(), OnMapReadyCallback {
             fusedLocationClient.lastLocation
                 .addOnSuccessListener { location: Location? ->
                     location?.let {
+                        onSuccess(location)
                         val currentLatLng = LatLng(location.latitude, location.longitude)
                         mMap.addMarker(MarkerOptions().position(currentLatLng).title("Current Location"))
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, DEFAULT_ZOOM))
