f85692d90c1dc7da12d11bec82161e641c00377e
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-07-16 12:08:44 +0800

    added NearbyPlacesResponse.kt

---
+++ app/src/main/java/com/example/gomommy/api/NearbyPlacesResponse.kt
@@ -0,0 +1,14 @@
+
+
+package com.example.gomommy.api
+
+import com.google.android.libraries.places.api.model.Place
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Data class encapsulating a response from the nearby search call to the Places API.
+ */
+data class NearbyPlacesResponse(
+   @SerializedName("results") val results: List<Place>
+)
+
