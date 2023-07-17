c7e1db7051da838888324fecf69d8f8596260715
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2023-07-17 20:22:47 +0800

    updated place kt

--- app/src/main/java/com/example/gomommy/model/Place.kt
+++ app/src/main/java/com/example/gomommy/model/Place.kt
@@ -1,20 +1,12 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
 
 package com.example.gomommy.model
 
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.ktx.utils.sphericalHeading
+
+import kotlin.math.cos
+import kotlin.math.sin
 
 /**
  * A model describing details about a Place (location, name, type, etc.).
@@ -36,8 +28,6 @@ data class Place(
         return this.id.hashCode()
     }
 }
-
-
 
 data class Geometry(
     val location: GeometryLocation
